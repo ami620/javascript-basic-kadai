35c2b993ad3d00000bb3d327bf1e830c1c253f9f
ami620 <rrte_620@outlook.jp>
2024-03-04 16:05:17 +0900

    third-commit

--- bmi.js
+++ bmi.js
@@ -1,27 +1,8 @@
-// 変数の宣言
-// let userHeight;
-
-// 値の代入
-// userHeight = 1.7;
-
-// 変数の宣言
-// let userWeight;
-
-// 値の代入
-// userWeight = 68;
+let userHeight = 1.7;
+let userWeight = 68;
 
 
-const userHeight = 1.7;
-const userWeight = 68;
-
-// 計算
-// console.log( userWeight % (userHeight * userHeight));
-
+// 丸め誤差対応のため、toFixedメソッドを使用
 console.log((userWeight / (userHeight * userHeight)).toFixed(2));
 
-
-
-// // 体重：68kg
-// // 身長：1.7m
-// // 計算式：[体重(kg)]÷[身長(m)×身長(m)]
 
